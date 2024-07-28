cde3d3ece26c42ab74ac874e7f11bd9c57f631c6
ubeyid oumer <ubeyidah@gmail.com>
2024-07-28 22:07:44 +0300

    complete header section ui

---
+++ src/components/Header.jsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="max-w-6xl mx-auto  p-4 flex justify-between items-center">
+      <div>
+        <Link href="/" className="font-bold text-lg text-lime-600">
+          MovieHex.
+        </Link>
+      </div>
+      <nav className="flex items-center gap-5 text-lime-900">
+        <Link
+          href="/"
+          className="hover:text-lime-600 transition-all duration-200"
+        >
+          <span className="sm:inline hidden">Home</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5 sm:hidden inline hover:bg-lime-100 hover:text-lime-800 p-1 rounded-full cursor-pointer transition-all duration-300 box-content"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        </Link>
+        <Link
+          href="/about"
+          className="hover:text-lime-600 transition-all duration-200"
+        >
+          <span className="sm:inline hidden">About</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5 sm:hidden inline hover:bg-lime-100 hover:text-lime-800 p-1 rounded-full cursor-pointer transition-all duration-300 box-content"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+            />
+          </svg>
+        </Link>
+        <button className="ml-8 max-sm:ml-0 hover:bg-lime-100 p-1 rounded-full cursor-pointer transition-all duration-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+            />
+          </svg>
+        </button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
