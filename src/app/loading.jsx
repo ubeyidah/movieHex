0200562ec31a9afa7584e352232e66063272af59
ubeyid oumer <ubeyidah@gmail.com>
2024-07-29 10:11:05 +0300

    create loading component

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <section className="flex items-center justify-center min-h-[70vh]">
+      <img src="loading.svg" alt="Loading..." />
+    </section>
+  );
+};
+
+export default loading;
--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,21 @@ const page = async ({ searchParams }) => {
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 1000 } }
   );
   const data = await res.json();
   if (!res.ok) {
     throw new Error("Faild to fetch movies");
   }
   const results = data.results;
-  return <section>page</section>;
+  return (
+    <section>
+      {results.map((movie) => (
+        <p key={movie.id}>{movie.title}</p>
+      ))}
+    </section>
+  );
 };
 
 export default page;
