530ed154caa07953781fb751b439ac0d1c6cb635
ubeyid oumer <ubeyidah@gmail.com>
2024-07-29 08:08:55 +0300

    complete about page ui

---
+++ src/app/about/page.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="max-w-6xl mx-auto px-5 pt-6">
+      <h1 className="text-lg font-semibold mb-2 text-center">
+        About Us - MovieHex
+      </h1>
+      <p>
+        Welcome to MovieHex, your ultimate destination for discovering and
+        exploring movies from around the world. Whether you're a casual viewer
+        or a dedicated cinephile, MovieHex offers a comprehensive database that
+        brings together the best of cinema right to your fingertips.
+      </p>
+      <h1 className="text-lg font-semibold my-2 text-center">Who We Are</h1>
+      <p>
+        At MovieHex, we are passionate about movies and believe in the power of
+        storytelling. Our team is dedicated to creating a user-friendly platform
+        that allows movie lovers to find information, ratings, reviews, and more
+        about their favorite films. From the latest blockbusters to timeless
+        classics, we strive to provide detailed and accurate information for
+        every movie in our database.
+      </p>
+      <h1 className="text-lg font-semibold my-2 text-center">Our Mission</h1>
+      <p>
+        Our mission at MovieHex is to make movie discovery and exploration an
+        enjoyable experience for everyone. We aim to be the go-to platform for
+        all things movies, providing a space where users can connect, share, and
+        celebrate their love for cinema.
+      </p>
+      <h1 className="text-lg font-semibold my-2 text-center">Join Us</h1>
+      <p>
+        We are constantly working to improve and expand our platform, and we
+        welcome feedback from our users. If you have any suggestions, questions,
+        or simply want to share your love for movies, please don't hesitate to
+        reach out to us.
+      </p>
+      <p>
+        Stay connected with us on social media for the latest updates, movie
+        news, and more. And don't forget to check out our project on GitHub:{" "}
+        <a
+          href="https://github.com/ubeyidah"
+          target="_blank"
+          className="text-blue-700"
+        >
+          github.com/ubeyidah
+        </a>
+        .
+      </p>
+      <p className="mt-2 text-center">
+        Thank you for choosing MovieHex. Let's embark on this cinematic journey
+        together!
+      </p>
+    </section>
+  );
+};
+
+export default About;
